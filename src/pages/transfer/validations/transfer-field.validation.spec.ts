417f54d5eb7689c639f0cb30e7c30cd400fca1c8
adriandcoding <theamr91@gmail.com>
2024-07-20 19:55:17 +0200

    implementando test y validaciones

--- src/pages/transfer/validations/transfer-field.validation.spec.ts
+++ src/pages/transfer/validations/transfer-field.validation.spec.ts
@@ -2,6 +2,11 @@ import {
   validateIBANField,
   REQUIRED_FIELD_MESSAGE,
   INVALID_IBAN_MESSAGE,
+  validateAccountIdField,
+  validateNameField,
+  validateAmountField,
+  validateConceptField,
+  INVALID_AMOUNT_MESSAGE,
 } from "./transfer-field.validation";
 
 describe("transfer-field.validation specs", () => {
@@ -29,6 +34,91 @@ describe("transfer-field.validation specs", () => {
       const value = "AL35202111090000000001234567";
       //act
       const result = validateIBANField(value);
+      //assert
+      expect(result.succeeded).toBeTruthy();
+    });
+  });
+  describe("validateAccountIdField", () => {
+    it("should return false when account id is empty", () => {
+      //arrange
+      const value = "";
+      //act
+      const result = validateAccountIdField(value);
+      //assert
+      expect(result.succeeded).toBeFalsy();
+      expect(result.message).toBe(REQUIRED_FIELD_MESSAGE);
+    });
+    it("should return true when the value is informed", () => {
+      //arrange
+      const value = "1";
+      //act
+      const result = validateAccountIdField(value);
+      //assert
+      expect(result.succeeded).toBeTruthy();
+    });
+  });
+  describe("validateNameField", () => {
+    it("should return false when name is empty", () => {
+      //arrange
+      const value = "";
+      //act
+      const result = validateNameField(value);
+      //assert
+      expect(result.succeeded).toBeFalsy();
+      expect(result.message).toBe(REQUIRED_FIELD_MESSAGE);
+    });
+    it("should return true when the value is informed", () => {
+      //arrange
+      const value = "John Doe";
+      //act
+      const result = validateNameField(value);
+      //assert
+      expect(result.succeeded).toBeTruthy();
+    });
+  });
+  describe("validateAmountField", () => {
+    it("should return false when amount is negative", () => {
+      //arrange
+      const value = -10;
+      //act
+      const result = validateAmountField(value);
+      //assert
+      expect(result.succeeded).toBeFalsy();
+      expect(result.message).toBe(INVALID_AMOUNT_MESSAGE);
+    });
+    it("should return false when amount is zero", () => {
+      //arrange
+      const value = 0;
+      //act
+      const result = validateAmountField(value);
+      //assert
+      expect(result.succeeded).toBeFalsy();
+      expect(result.message).toBe(INVALID_AMOUNT_MESSAGE);
+    });
+    it("should return true when amount is positive", () => {
+      //arrange
+      const value = 10;
+      //act
+      const result = validateAmountField(value);
+      //assert
+      expect(result.succeeded).toBeTruthy();
+    });
+  });
+  describe("validateConceptField", () => {
+    it("should return false when concept is empty", () => {
+      //arrange
+      const value = "";
+      //act
+      const result = validateConceptField(value);
+      //assert
+      expect(result.succeeded).toBeFalsy();
+      expect(result.message).toBe(REQUIRED_FIELD_MESSAGE);
+    });
+    it("should return true when the value is informed", () => {
+      //arrange
+      const value = "Transfer";
+      //act
+      const result = validateConceptField(value);
       //assert
       expect(result.succeeded).toBeTruthy();
     });
--- src/pages/transfer/validations/transfer-field.validation.ts
+++ src/pages/transfer/validations/transfer-field.validation.ts
@@ -8,6 +8,7 @@ import { FieldValidation } from "../transfer.vm";
 
 export const REQUIRED_FIELD_MESSAGE = "Este campo es obligatorio";
 export const INVALID_IBAN_MESSAGE = "El IBAN ingresado no es válido";
+export const INVALID_AMOUNT_MESSAGE = "La cantidad debe de ser mayor que 0";
 const buildValidationFalseResponse = (message: string): FieldValidation => ({
   succeeded: false,
   message,
@@ -22,6 +23,31 @@ export const validateIBANField = (value: string): FieldValidation => {
   }
   if (!isValidIban(value)) {
     return buildValidationFalseResponse(INVALID_IBAN_MESSAGE);
+  }
+  return buildValidationTrueResponse();
+};
+
+export const validateAccountIdField = (value: string): FieldValidation => {
+  if (!isFieldInformed(value)) {
+    return buildValidationFalseResponse(REQUIRED_FIELD_MESSAGE);
+  }
+  return buildValidationTrueResponse();
+};
+export const validateNameField = (value: string): FieldValidation => {
+  if (!isFieldInformed(value)) {
+    return buildValidationFalseResponse(REQUIRED_FIELD_MESSAGE);
+  }
+  return buildValidationTrueResponse();
+};
+export const validateAmountField = (value: number): FieldValidation => {
+  if (!isPositiveNumber(value)) {
+    return buildValidationFalseResponse(INVALID_AMOUNT_MESSAGE);
+  }
+  return buildValidationTrueResponse();
+};
+export const validateConceptField = (value: string): FieldValidation => {
+  if (!isFieldInformed(value)) {
+    return buildValidationFalseResponse(REQUIRED_FIELD_MESSAGE);
   }
   return buildValidationTrueResponse();
 };
